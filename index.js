a64a52f8ae7293b841a56fa91a78e667df1656a4
Diego Rodrigo <diego.rrfc@gmail.com>
2020-02-02 18:35:47 -0300

    aula 37

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send("form recebido" + titulo + "des" + descricao);
 });
 
 app.listen(8000, () => console.log("rodando"));
